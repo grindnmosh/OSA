956ef0a08229fba4c98f5d9369629ca3584fa52b
Robert Warren <grindnmosh@gmail.com>
2014-11-12 22:49:11 -0700

    Delete all function and initial screen for projects done

--- app/src/main/java/com/grinddesign/osa/PMainActivity.java
+++ app/src/main/java/com/grinddesign/osa/PMainActivity.java
@@ -1,6 +1,7 @@
 package com.grinddesign.osa;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,16 +10,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 
 public class PMainActivity extends Activity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+
+    Button projButt;
 
     ArrayList<ProjectObject> pData;
     String[] loads;
@@ -27,10 +34,13 @@ public class PMainActivity extends Activity implements AdapterView.OnItemClickLi
     String proj;
     String day;
     String hour;
+    String p;
     int i;
+    int c;
     ArrayList<String> projects = new ArrayList<String>();
     ArrayList<String> days = new ArrayList<String>();
     ArrayList<String> hours = new ArrayList<String>();
+    ArrayList<ProjectObject> crazyGlue = new ArrayList<ProjectObject>();
 
     static ArrayAdapter<String> projAdapter;
 
@@ -42,6 +52,8 @@ public class PMainActivity extends Activity implements AdapterView.OnItemClickLi
         setContentView(R.layout.activity_pmain);
 
         lv = (ListView) findViewById(R.id.plistView);
+        projButt = (Button) findViewById(R.id.projButt);
+        projButt.setOnClickListener(myhandler);
 
         try {
             Log.i("step", "1");
@@ -67,6 +79,8 @@ public class PMainActivity extends Activity implements AdapterView.OnItemClickLi
 
         if (pData != null) {
 
+            projButt.setVisibility(View.INVISIBLE);
+
             for (i = 0; i < pData.size(); i++) {
                 proj = "";
                 ProjectObject data = pData.get(i);
@@ -80,6 +94,10 @@ public class PMainActivity extends Activity implements AdapterView.OnItemClickLi
 
 
             }
+        }
+        else
+        {
+            projButt.setVisibility(View.VISIBLE);
         }
 
         s = (Spinner) findViewById(R.id.pspinner);
@@ -133,6 +151,20 @@ public class PMainActivity extends Activity implements AdapterView.OnItemClickLi
             Intent pAddNew = new Intent(this, PAddActivity.class);
             this.startActivity(pAddNew);
         }
+        else if (id == R.id.action_delall) {
+            deleteFile("proj.dat");
+            projects = new ArrayList<String>();
+            projAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, projects);
+            lv.setOnItemClickListener(this);
+            lv.setOnItemLongClickListener(this);
+
+            lv.setAdapter(projAdapter);
+
+            projButt.setVisibility(View.VISIBLE);
+
+        }
+
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -151,7 +183,80 @@ public class PMainActivity extends Activity implements AdapterView.OnItemClickLi
     }
 
     @Override
-    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+        try {
+            Log.i("step", "1");
+            FileInputStream fis = openFileInput("proj.dat");
+            Log.i("step", "2");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Log.i("read", "trying to read saved file");
+            if (pData == null) {
+                pData = new ArrayList<ProjectObject>();
+                Log.i("TEST", "TEST");
+            }
+            pData = (ArrayList<ProjectObject>) ois.readObject();
+            Log.i("read", String.valueOf(pData));
+            crazyGlue.addAll(pData);
+
+            ois.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("check kill", "Click Click Boom");
+        for (i = 0; i < crazyGlue.size(); i++) {
+            p = projects.get(position);
+            Log.i("check class", String.valueOf(p));
+            for (c = 0; c < crazyGlue.size(); c++) {
+                ProjectObject subData = crazyGlue.get(c);
+                String pCurr = subData.getName();
+                Log.i("read", String.valueOf(p));
+                if (p.equals(pCurr)) {
+                    crazyGlue.remove(subData);
+                }
+            }
+
+        }
+
+        try {
+            Log.i("jObj2", "am I here");
+            FileOutputStream fos = openFileOutput("ass.dat", Context.MODE_PRIVATE);
+
+            // Wrapping our file stream.
+            Log.i("jObj2", "am I here2");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            // Writing the serializable object to the file
+            Log.i("jObj2", "am I here3");
+            pData = new ArrayList<ProjectObject>();
+
+            pData.addAll(crazyGlue);
+
+            Log.i("jObj2", String.valueOf(pData));
+            oos.writeObject(pData);
+            // Closing our object stream which also closes the wrapped stream.
+
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("jObj2", "Not Doing It");
+        }
+
+
+
+
+
+        projects.remove(position);
+        projAdapter.notifyDataSetChanged();
         return false;
     }
+
+    View.OnClickListener myhandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent newProj = new Intent(PMainActivity.this, PAddActivity.class);
+            startActivity(newProj);
+        }
+    };
 }
